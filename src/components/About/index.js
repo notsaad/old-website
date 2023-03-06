43a9e82b6b566ca3e6f4ab4fe1abfa39dfa083cb
notsaad <saadmazharr@gmail.com>
2023-03-06 11:24:19 -0500

    added about and contact pages

---
+++ src/components/About/index.js
@@ -0,0 +1,76 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGitAlt, faHtml5, faJava, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const aboutArray = "About Me".split("")
+
+const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000)
+
+        return () => clearTimeout(timer)
+    }, []);
+
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={aboutArray}
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                    I'm an extremely ambitious developer looking for internships, and opportunities to broaden my skillset
+                    by working on challenging, new projects.
+                </p>
+                <p>
+                    I would describe myself as curious, confident, and a natural leader who is always looking to learn new things
+                    and implement them into my upcoming projects.
+                </p>
+                <p>
+                    I am finishing up my 2nd year of my Computer Science Major here at the University of Ottawa, however,
+                    I have been coding and tinkering with technology as early as I can remember.
+                </p>
+                <p>
+                    Feel free to contact me, or connect on LinkedIn for any questions, inquiries, opportunities, or
+                    if you just want to chat!
+                </p>
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faPython} color="#ffde57" />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJava} color="#1fbde6" />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contact/index.js
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import emailjs from '@emailjs/browser'
+
+const contactArray = "Contact Me".split("")
+const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000)
+
+        return () => clearTimeout(timer)
+    }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_h0vroti',
+                'template_n6piz06',
+                refForm.current,
+                'tvIiG9kTOr1okutYb'
+            )
+            .then(
+                () => {
+                    alert('Message Sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Message failed to send, please try again.')
+                }
+            )
+    }
+
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters 
+                            letterClass={letterClass}
+                            strArray={contactArray}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I am seeking internship opportunities for the 2023 summer and fall terms,
+                        and am open to 4/8 month positions. However, feel free to contact me for any other inquiries.
+                    </p>
+                    <div className='contact-form'>
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Name' required />
+                                </li>
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Email' required />
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name='subject' required />
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required></textarea>
+                                </li>
+                                <li>
+                                    <input type='submit' className='flat-button' value='SEND' />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contact
